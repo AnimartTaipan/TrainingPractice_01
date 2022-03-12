f70bcb95aa629b5a8e75b9227bf69c37175bdd81
Animart Taipan <83275233+AnimartTaipan@users.noreply.github.com>
2022-03-12 03:13:51 +0300

    Add files via upload

---
+++ CLV_Task_02/CLV_Task_02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLV_Task_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void Print(string word)
+            {
+                Console.WriteLine(word);
+            } 
+            
+            string exit = "";
+
+            int tries = 0;
+
+            while (tries<3)
+            {
+                if (exit == "Exit") { break; }
+                Print("Программа выполняется.");
+                Print("Если вы хотите закончить выполнение программы - напишите 'Exit'");
+                Print("Если вы хотите продолжить выполнение программы - нажмите 'Enter'");
+                tries++;
+                exit = Console.ReadLine();
+            }
+        }
+    }
+}
+
